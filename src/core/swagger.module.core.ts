fd9f8520a2abfe14ea2a53da5a561169b16d238f
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-05-05 23:54:21 +0300

    Update swagger.module.core.ts

--- src/core/swagger.module.core.ts
+++ src/core/swagger.module.core.ts
@@ -10,30 +10,16 @@ export class SwaggerModuleCore {
     const builder = new DocumentBuilder();
 
     builder
-      .setTitle('WAHA - WhatsApp HTTP API')
+      .setTitle('MEGA - API')
       .setDescription(
-        'WhatsApp HTTP API that you can run in a click!<br/>' +
+        'WhatsApp API<br/>' +
           '<br/>' +
           'Learn more:' +
           '<ul>' +
-          '<li><a href="https://waha.devlike.pro/" target="_blank">Documentation</a></li>' +
-          '<li><a href="https://waha.devlike.pro/docs/how-to/engines/#features" target="_blank">Supported features in engines</a></li>' +
-          '<li><a href="https://github.com/devlikeapro/whatsapp-http-api" target="_blank">GitHub - WAHA Core</a></li>' +
-          '<li><a href="https://github.com/devlikeapro/whatsapp-http-api-plu' + // Separate line to pass pre-commit check
-          's" target="_blank">GitHub - WAHA Plus</a></li>' +
-          '<li><b>NEW!</b> - <a href="/dashboard">WAHA Dashboard</a></li>' +
-          '<li><b>NEW!</b> - <a href="https://waha.devlike.pro/docs/how-to/waha-dashboard">WAHA Dashboard Documentation</a></li>' +
-          '</ul>' +
-          '<p>Support the project and get WAHA Plus version!</p>' +
-          '<ul>' +
-          '<li><a href="https://waha.devlike.pro/docs/how-to/plu' + // Separate line to pass pre-commit check
-          's-version/" target="_blank">WAHA Plus</a></li>' +
-          '<li><a href="https://patreon.com/wa_http_api/" target="_blank">Patreon</a></li>' +
-          '<li><a href="https://boosty.to/wa-http-api/" target="_blank">Boosty</a></li>' +
-          '<li><a href="https://portal.devlike.pro/" target="_blank">Patron Portal</a></li>' +
+          '<li><a href="https://www.facebook.com/profile.php?id=61553843708296" target="_blank">API BY ELFANAAN</a></li>' +
           '</ul>',
       )
-      .setExternalDoc('WAHA', 'https://waha.devlike.pro/')
+      .setExternalDoc('ELFANAAN', 'https://www.facebook.com/profile.php?id=61553843708296')
       .setVersion(VERSION.version)
       .addTag('sessions', 'Control WhatsApp sessions')
       .addTag('auth', 'Authentication')
@@ -43,16 +29,6 @@ export class SwaggerModuleCore {
         'status',
         'Status (aka stories) methods. <b>NOWEB</b> engine only!',
       )
-      .addTag('chats', `Chats methods`)
-      .addTag(
-        'contacts',
-        `Contacts methods.<br>
-                Use phone number (without +) or phone number and \`@c.us\` at the end as \`contactId\`.<br>
-                'E.g: \`12312312310\` OR \`12312312310@c.us\`<br>`,
-      )
-      .addTag('groups', `Groups methods.<br>`)
-      .addTag('presence', `Presence information`)
-      .addTag('other', 'Other methods')
       .addApiKey({
         type: 'apiKey',
         description: 'Your secret api key',
@@ -95,7 +71,7 @@ export class SwaggerModuleCore {
     );
     document = this.configureWebhooks(document, webhooks);
     SwaggerModule.setup('', app, document, {
-      customSiteTitle: 'WAHA - WhatsApp HTTP API',
+      customSiteTitle: 'MEGA - API',
     });
   }
 
